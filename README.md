94fa772c98295d295246d5d8fe6c28e02c2b9bcb
eggbeard <michael.hodgson@gmail.com>
2023-08-04 09:38:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# a6chkf-pgyj4k
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/a6chkf-pgyj4k)
---
+++ src/example/button-toggle-overview-example.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
+/**
+ * @title Basic button-toggles
+ */
+@Component({
+  selector: 'button-toggle-overview-example',
+  templateUrl: 'button-toggle-overview-example.html',
+  standalone: true,
+  imports: [MatButtonToggleModule],
+})
+export class ButtonToggleOverviewExample {}
+
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
